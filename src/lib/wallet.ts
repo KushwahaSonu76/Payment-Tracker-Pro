a9309116729b20f4e43b07f333f9324b02049095
ritesh kushwaha <sonukushwaha821304@gmail.com>
2026-07-03 22:56:22 +0530

    feat: Configure StellarWalletsKit with Freighter, xBull, Albedo

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,11 +1,16 @@
-import {
-  StellarWalletsKit,
-  WalletNetwork,
-  allowAllModules,
-} from "@creit.tech/stellar-wallets-kit";
+import { StellarWalletsKit, Networks } from '@creit.tech/stellar-wallets-kit';
+import { FreighterModule } from '@creit.tech/stellar-wallets-kit/modules/freighter';
+import { xBullModule } from '@creit.tech/stellar-wallets-kit/modules/xbull';
+import { AlbedoModule } from '@creit.tech/stellar-wallets-kit/modules/albedo';
 
-export const kit = new StellarWalletsKit({
-  network: WalletNetwork.TESTNET,
-  selectedWalletId: "freighter",
-  modules: allowAllModules(),
+StellarWalletsKit.init({
+  network: Networks.TESTNET,
+  selectedWalletId: 'freighter',
+  modules: [
+    new FreighterModule(),
+    new xBullModule(),
+    new AlbedoModule(),
+  ]
 });
+
+export const kit = StellarWalletsKit;
